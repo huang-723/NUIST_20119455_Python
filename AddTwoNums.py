5fd9d4be0a0b1c45aa0928c046020c56b9fcc524
hyfgg <hyfgg20070723@qq.com>
2026-03-23 08:23:55 +0800

    Update to Version3 AddTwoNums.py

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,4 +1,8 @@
-a = input("First number: ")
-b = input("Second number: ")
-res = float(a) + float(b)
+def add(a, b):
+    return float(a) + float(b)
+
+a = input("First Number: ")
+b = input("Second Number: ")
+res = add(a, b)
+print("The Answer is:")
 print(res)
